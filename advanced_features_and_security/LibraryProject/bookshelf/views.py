9549b14bd3fba54354e68a3cb7b1ec59a1158bb7
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-11-15 16:32:22 +0300

    Add ExampleForm import and form_example view to bookshelf

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .forms import ExampleForm
 
 # Create your views here.
+
+def form_example(request):
+    """
+    View for demonstrating secure form handling with Django.
+    
+    Security features demonstrated:
+    - CSRF token validation
+    - Form input validation
+    - XSS prevention via auto-escaping
+    """
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Process form data safely
+            name = form.cleaned_data.get('name')
+            email = form.cleaned_data.get('email')
+            message = form.cleaned_data.get('message')
+            
+            # Data is sanitized and safe to use
+            return render(request, 'bookshelf/form_example.html', {
+                'form': ExampleForm(),
+                'success': True
+            })
+    else:
+        form = ExampleForm()
+    
+    return render(request, 'bookshelf/form_example.html', {'form': form})
