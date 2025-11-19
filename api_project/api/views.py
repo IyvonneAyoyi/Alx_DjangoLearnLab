f70c42715834b999ff27fc172f21410c5cbd2fb7
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-11-19 17:19:27 +0300

    Implement BookViewSet in api/views.py using ModelViewSet for list, create, retrieve, update, and delete and Configure Router

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,17 @@
-from rest_framework import generics
+from rest_framework import generics,viewsets
 from .models import Book
 from .serializers import BookSerializer
 
+#List only view
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+#Implement CRUD
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    Provides list, create, retrieve, update, and delete actions
+    for the Book model.
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
