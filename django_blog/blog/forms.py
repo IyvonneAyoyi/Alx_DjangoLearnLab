e9a855494625566667a8d66b80c2e865d71fe76f
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-12-06 23:33:17 +0300

    Add CommentForm to handle creation and updating of comments

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment  # <-- import Comment model
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -14,3 +14,17 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content"]
+
+# ----------------------------
+# Comment Form
+# ----------------------------
+class CommentForm(forms.ModelForm):
+    content = forms.CharField(
+        widget=forms.Textarea(attrs={'rows': 3, 'placeholder': 'Write your comment here...'}),
+        max_length=1000,
+        label=''
+    )
+
+    class Meta:
+        model = Comment
+        fields = ['content']
