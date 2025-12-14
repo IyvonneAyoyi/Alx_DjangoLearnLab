87dae543c725cb30aa9c44d842e0da56bc35b4b6
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-12-14 15:04:08 +0300

    Add follow management URL patterns to accounts

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import RegisterView, LoginView,UserProfileView
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import RegisterView, LoginView,UserProfileView, FollowUserView, UnfollowUserView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow_user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow_user'),
+
 ]
