b849efeb1e1bc081adc03bb87bc263cf0d276cb1
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-12-14 14:54:38 +0300

    Add following field to CustomUser model

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -5,3 +5,13 @@ class CustomUser(AbstractUser):
     bio = models.TextField(blank=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
     followers = models.ManyToManyField('self', symmetrical=False, blank=True)
+    
+    following = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='followed_by', 
+        blank=True
+    )
+    
+    def __str__(self):
+        return self.username
